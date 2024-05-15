c286cdfb068ee26881884edeed1a9446e8e12da9
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-05-15 14:56:53 -0300

    frontEnd-0.0.1.7

--- components/Servicos/ComponenteMedicos/styles.tsx
+++ components/Servicos/ComponenteMedicos/styles.tsx
@@ -7,14 +7,38 @@ export const styles = StyleSheet.create({
     height: 120,
     alignSelf: 'center',
     marginTop: 25,
-    padding: 15,
+    padding: 10,
     borderRadius: 10,
     elevation: 5,
+    justifyContent: 'center',
   },
   imagemIcone: {
-
+    marginRight: 15,
+    backgroundColor: 'lightgray',
+    width: 50,
+    height: 60,
+    borderRadius: 10,
+    borderWidth: 0.4,
+    borderColor: 'gray',
   },
-  tituloMedico: {
+  areaTextos: {
     position: 'absolute',
+    backgroundColor: 'gray',
+    width: '80%',
+    marginLeft: 70,
+    height: '100%',
+    padding: 10,
+    borderRadius: 8,
+    flexDirection: 'column',
+  },
+  textoNomeMedico: {},
+  textoEndereco: {},
+  nomeMedico: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 12,
+  },
+  nomeEndereco: {
+    color: '#fff',
   },
 });
