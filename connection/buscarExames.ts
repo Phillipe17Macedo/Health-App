8493f0955330cc7348fff8b61b59d70bc83b5b78
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-05-21 20:55:51 -0300

    frontEnd-0.0.4.4

--- connection/buscarExames.ts
+++ connection/buscarExames.ts
@@ -1,8 +1,28 @@
 import { ref, get, child } from "firebase/database";
 import { database } from "@/connection/firebase";
 
+interface Exames {
+  nome: string;
+}
 
-
-export async function buscarExames() {
-    
+export async function buscarExames(): Promise<
+  { label: string; value: string }[]
+> {
+  const dbRef = ref(database);
+  try {
+    const snapshot = await get(child(dbRef, "exames"));
+    if (snapshot.exists()) {
+      const exames: Record<string, Exames> = snapshot.val();
+      return Object.keys(exames).map((key) => ({
+        label: exames[key].nome,
+        value: key,
+      }));
+    } else {
+      console.log("Não foi possível encontrar os Exames");
+      return [];
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
