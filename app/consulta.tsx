e1e59c97cb92a17503ce903c2a4b5da5f7eced75
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-05-20 21:09:35 -0300

    frontEnd-0.0.3.9

--- app/consulta.tsx
+++ app/consulta.tsx
@@ -1,17 +1,21 @@
-import React, { useState } from 'react';
-import { View } from 'react-native';
-import { styles } from '../styles/StylesServicosPage/StylesConsultaPage/styles';
-import { HeaderConsulta } from '@/components/Consulta/HeaderConsulta/Header';
-import Especialidade from '@/components/Consulta/DropDownEspecialidade/Especialidade';
-import Medico from '@/components/Consulta/DropDownMedico/Medico';
+import React, { useState } from "react";
+import { SafeAreaView, ScrollView, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
+import { styles } from "../styles/StylesServicosPage/StylesConsultaPage/styles";
+import { HeaderConsulta } from "@/components/Consulta/HeaderConsulta/Header";
+import { SeachBar } from "@/components/Servicos/SeachBar/SearchBar";
+import Especialidade from "@/components/Consulta/DropDownEspecialidade/Especialidade";
+import Medico from "@/components/Consulta/DropDownMedico/Medico";
 
 export default function Consulta() {
   const [especialidadeId, setEspecialidadeId] = useState<string | null>(null);
 
   return (
     <View style={styles.container}>
-        <HeaderConsulta/>
+      <StatusBar style="auto" />
+        <HeaderConsulta />
+        <SeachBar />
         <Especialidade EspecialidadeCarregada={setEspecialidadeId} />
         <Medico especialidadeId={especialidadeId} />
     </View>
