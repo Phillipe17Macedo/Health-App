0930450752aed2c8b36b3d4712a7f2f685fb34b1
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-05-24 19:42:28 -0300

    frontend-Exames-0.0.4

--- components/Exame/DropDownExame/styles.tsx
+++ components/Exame/DropDownExame/styles.tsx
@@ -1,5 +1,39 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {}
+  container: {
+    marginLeft: 35,
+    marginRight: 35,
+    marginTop: 25,
+    marginBottom: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  dropdown: {
+    backgroundColor: '#DFF2ED',
+    borderColor: '#65A693',
+    borderWidth: 0.5,
+    elevation: 2,
+    borderRadius: 15,
+  },
+  textoDropdown: {
+    color: '#025940',
+    fontSize: 16,
+  },
+  dropDownContainerStyle: {
+    backgroundColor: '#DFF2ED',
+    borderColor: '#65A693',
+    borderWidth: 0.5,
+    elevation: 2,
+    borderRadius: 15,
+  },
+  itensLista: {
+    color: '#025940',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  itemSelecionado: {
+    fontWeight: 'bold',
+    color: '#8CBF1F',
+  },
 });
--- components/Exame/DropDownLaboratorio/DropDownLaboratorio.tsx
+++ components/Exame/DropDownLaboratorio/DropDownLaboratorio.tsx
@@ -3,16 +3,16 @@ import { View } from "react-native";
 import DropDownPicker from "react-native-dropdown-picker";
 import { styles } from "./styles";
 
-export default function DropDownExame() {
+export default function DropDownLaboratorio() {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState(null);
   const [items, setItems] = useState([
-    { label: "Exame 1", value: "exame1" },
-    { label: "Exame 2", value: "exame2" },
-    { label: "Exame 3", value: "exame3" },
-    { label: "Exame 4", value: "exame4" },
-    { label: "Exame 5", value: "exame5" },
-    { label: "Exame 6", value: "exame6" },
+    { label: "Laboratorio 1", value: "laboratorio1" },
+    { label: "Laboratorio 2", value: "laboratorio2" },
+    { label: "Laboratorio 3", value: "laboratorio3" },
+    { label: "Laboratorio 4", value: "laboratorio4" },
+    { label: "Laboratorio 5", value: "laboratorio5" },
+    { label: "Laboratorio 6", value: "laboratorio6" },
   ]);
 
   return (
@@ -25,14 +25,14 @@ export default function DropDownExame() {
           setOpen={setOpen}
           setValue={setValue}
           setItems={setItems}
-          placeholder="Selecione um Exame"
+          placeholder="Selecione um Laboratorio"
           style={styles.dropdown}
           placeholderStyle={styles.textoDropdown}
           dropDownContainerStyle={styles.dropDownContainerStyle}
           listItemLabelStyle={styles.itensLista}
           selectedItemLabelStyle={styles.itemSelecionado}
-          zIndex={3000}
-          zIndexInverse={1000}
+          zIndex={2000}
+          zIndexInverse={2000}
         />
       </View>
     </>
