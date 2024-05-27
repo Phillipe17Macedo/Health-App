0ca441393d9d80effc61b6a1f732eb3ad12e46a7
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-05-27 20:57:28 -0300

    frontend-Consultas-0.0.9

---
+++ connection/buscarUsuarioPorCPF.ts
@@ -0,0 +1,17 @@
+import { ref, get, child } from "firebase/database";
+import { database } from "@/connection/firebase";
+
+export async function buscarUsuarioLogadoPorCPF(cpf: string) {
+  const dbRef = ref(database);
+  try {
+    const snapshot = await get(child(dbRef, `usuarios/${cpf}`));
+    if (snapshot.exists()) {
+      return snapshot.val();
+    } else {
+      throw new Error("Usuário não encontrado");
+    }
+  } catch (error) {
+    console.error("Erro ao buscar usuário logado:", error);
+    throw error;
+  }
+}
