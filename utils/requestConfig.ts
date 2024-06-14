197d1b48139062ad1a8977465eef92c3f4179368
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-06-14 14:08:42 -0300

    alt-consultas-0.9

--- utils/requestConfig.ts
+++ utils/requestConfig.ts
@@ -28,7 +28,21 @@ export async function buscarDependentes(idAderente: number): Promise<any> {
 export async function buscarMedicosEspecialidade(especialidadeId: string): Promise<any> {
   try {
     const response = await axiosConfig.get(`/Medico/GetMedicosEspecialidade/${especialidadeId}`);
-    return response.data;
+    const medicos = response.data.data;
+
+    // Adicionando dias e horários fictícios
+    const diasFicticios = ["segunda", "terça", "quarta", "quinta", "sexta"];
+    const horariosFicticios = ["09:00", "10:00", "11:00", "14:00", "15:00", "16:00"];
+
+    const medicosComHorarios = medicos.map((medico: any) => ({
+      ...medico,
+      diasAtendimento: diasFicticios.reduce((acc: any, dia: string) => {
+        acc[dia] = horariosFicticios;
+        return acc;
+      }, {}),
+    }));
+
+    return { success: true, data: medicosComHorarios };
   } catch (error) {
     console.error("Erro ao buscar médicos por especialidade:", error);
     throw error;
