a4f72fc8a688098f04ee63c2e62a84a32f493c36
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-06-18 10:24:09 -0300

    guia-consulta-0.4

--- app/guiaConsulta.tsx
+++ app/guiaConsulta.tsx
@@ -3,12 +3,13 @@ import { View } from 'react-native';
 
 import { styles } from '../styles/GuiaConsulta/styles';
 import { HeaderGuiaConsulta } from '@/components/GuiaConsulta/HeaderGuiaConsulta/Header';
+import { DicaGuiaConsulta } from '../components/GuiaConsulta/ComponenteDicaSolicitacao/DicaGuiaConsulta';
 
 export default function GuiaConsulta() {
   return (
     <View style={styles.container}>
         <HeaderGuiaConsulta/>
-        
+        <DicaGuiaConsulta/>
     </View>
   );
 }
