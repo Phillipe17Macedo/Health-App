e3b6cc8acb9001ebb88f975efabbd48075a0b37d
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-06-24 09:59:50 -0300

    alt-screen-index-0.3

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Image } from "react-native";
+import { Link } from "expo-router";
+import { FontAwesome6 } from '@expo/vector-icons';
+import { styles } from "../styles/StylesIndexPage/styles";
 
-import { styles } from '../styles/StylesIndexPage/styles';
-
-export default function IndexGif() {
+export default function Index() {
   return (
     <View style={styles.container}>
-        <Text>Teste Tela do Gif</Text>
+      <View style={[styles.containerImage]}>
+        <Image
+          source={require("../assets/images/logo-aserpa/logo-animated.gif")}
+          style={[styles.imageStyle]}
+        />
+      </View>
+      <View style={[styles.areaButton]}>
+        <View style={[styles.containerButton]} >
+          <Link href={'/login'} >
+            <FontAwesome6 name="circle-arrow-right" size={53} color="#8CBF1F" />
+          </Link>
+        </View>
+      </View>
     </View>
   );
 }
--- styles/StylesIndexPage/styles.tsx
+++ styles/StylesIndexPage/styles.tsx
@@ -1,7 +1,32 @@
-import { StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
+
+const { height } = Dimensions.get('screen');
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+  },
+  containerImage: {
+    alignSelf: 'center',
+    marginTop: height * 0.2
+  },
+  imageStyle: {
+    resizeMode: 'contain',
+    height: 280,
+  },
+  areaButton: {
+    //backgroundColor: 'red',
+    width: '100%',
+    height: 70,
+    marginTop: height * 0.15,
+    justifyContent: 'center'
+  },
+  containerButton: {
+    //backgroundColor: 'gray',
+    height: '100%', 
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    paddingRight: 25,
   }
 });
