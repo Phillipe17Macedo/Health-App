250bb2821805c75e071eda56ccb3d2157e6a250f
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-06-24 10:23:28 -0300

    alt-screen-index-0.6

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Image } from "react-native";
 import { Link } from "expo-router";
 import { FontAwesome6 } from "@expo/vector-icons";
 import { styles } from "../styles/StylesIndexPage/styles";
 
 export default function Index() {
+  const [botaoVisivel, setBotaoVisivel] = useState(false);
+
+  useEffect(() => {
+    const temporizador = setTimeout(() => {
+      setBotaoVisivel(true);
+    }, 3000);
+
+    return () => clearTimeout(temporizador);
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={[styles.containerImage]}>
@@ -12,17 +22,19 @@ export default function Index() {
           source={require("../assets/images/logo-aserpa/logo-animated.gif")}
           style={[styles.imageStyle]}
         />
-        <View style={[styles.areaButton]}>
-          <View style={[styles.containerButton]}>
-            <Link href={"/login"}>
-              <FontAwesome6
-                name="circle-arrow-right"
-                size={54}
-                color="#8CBF1F"
-              />
-            </Link>
+        {botaoVisivel && (
+          <View style={[styles.areaButton]}>
+            <View style={[styles.containerButton]}>
+              <Link href={"/login"}>
+                <FontAwesome6
+                  name="circle-arrow-right"
+                  size={54}
+                  color="#8CBF1F"
+                />
+              </Link>
+            </View>
           </View>
-        </View>
+        )}
       </View>
     </View>
   );
