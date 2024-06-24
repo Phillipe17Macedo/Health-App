bd9b023b25740b16c7cf8830827919eb49527f86
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-06-24 09:16:57 -0300

    alt-screen-index-0.1

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,8 @@ export const unstable_settings = {
 export default function RootLayout() {
   return (
     <Stack>
-        <Stack.Screen name="index" options={{headerShown: false }} />
+        <Stack.Screen name="index" options={{headerShown: false}} />
+        <Stack.Screen name="login" options={{headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="perfil" options={{ headerShown: false }} />
         <Stack.Screen name="consulta" options={{ headerShown: false }} />
---
+++ app/index,.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { styles } from '../styles/StylesIndexPage/styles';
+
+export default function IndexGif() {
+  return (
+    <View style={styles.container}>
+        <Text>Teste Tela do Gif</Text>
+    </View>
+  );
+}
