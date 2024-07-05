4145c21445846fee072c8033b7b95673b64e5503
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-07-05 15:26:48 -0300

    correcao-appnao-vou-usar-o-firebase

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,8 @@
 import React, { useEffect } from "react";
-import { View, Image, Alert, Platform } from "react-native";
+import { View, Image } from "react-native";
 import { useRouter } from "expo-router";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { styles } from "../styles/StylesIndexPage/styles";
-import messaging from '@react-native-firebase/messaging';
 
 export default function Index() {
   const router = useRouter();
@@ -32,37 +31,6 @@ export default function Index() {
 
     verificarExibicaoInicial();
   }, [router]);
-
-  messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-    console.log("Message handled in the background!", remoteMessage);
-  });
-
-  async function requestUserPermission() {
-    if (Platform.OS === "android") {
-      await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS
-      );
-    } else {
-      const authStatus = await messaging().requestPermission();
-      const enabled =
-        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
-      if (enabled) {
-        console.log("Authorization status:", authStatus);
-      }
-    }
-  }
-
-  useEffect(() => {
-    requestUserPermission();
-  }, []);
-
-  useEffect(() => {
-    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
-      Alert.alert("A new FCM message arrived!", JSON.stringify(remoteMessage));
-    });
-    return unsubscribe;
-  }, []);
 
   return (
     <View style={styles.container}>
