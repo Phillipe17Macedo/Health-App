33fafc7027bc652ba553518dc2125143867eb013
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-07-08 09:43:01 -0300

    configurando-onesignal-0.1

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { View, Image } from "react-native";
 import { useRouter } from "expo-router";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { styles } from "../styles/StylesIndexPage/styles";
-import { LogLevel, OneSignal } from "react-native-onesignal";
+import { OneSignal, LogLevel } from "react-native-onesignal";
 
 export default function Index() {
   const router = useRouter();
@@ -32,21 +32,20 @@ export default function Index() {
 
     verificarExibicaoInicial();
 
-    // Remove this method to stop OneSignal Debugging
     OneSignal.Debug.setLogLevel(LogLevel.Verbose);
 
-    // OneSignal Initialization
+    let externalId = "123456789"
+
     OneSignal.initialize("1da038ce-7a5a-42f2-88c1-a119b0d94c29");
 
-    // requestPermission will show the native iOS or Android notification permission prompt.
-    // We recommend removing the following code and instead using an In-App Message to prompt for notification permission
     OneSignal.Notifications.requestPermission(true);
 
-    // Method for listening for notification clicks
     OneSignal.Notifications.addEventListener('click', (event) => {
       console.log('OneSignal: notification clicked:', event);
     });
 
+    OneSignal.login(externalId);
+    
   }, [router]);
 
   return (
