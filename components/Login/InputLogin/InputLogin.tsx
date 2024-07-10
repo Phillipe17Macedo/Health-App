9ad544ef0f351f31c7efe0888edd01ca4c2a0fd6
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-07-10 15:07:37 -0300

    sucess-autenticação-bio-face

--- components/Login/InputLogin/InputLogin.tsx
+++ components/Login/InputLogin/InputLogin.tsx
@@ -1,13 +1,21 @@
 import React, { useEffect, useState } from "react";
-import { TextInput, View, TouchableOpacity, Text, Alert, Dimensions, ActivityIndicator } from "react-native";
+import {
+  TextInput,
+  View,
+  TouchableOpacity,
+  Text,
+  Alert,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { FontAwesome6 } from '@expo/vector-icons';
+import { FontAwesome6 } from "@expo/vector-icons";
 import { Checkbox } from "react-native-paper";
 import { styles } from "./styles";
 import { buscarAderente } from "@/utils/requestConfig";
 import { useRouter } from "expo-router";
 import { AxiosError } from "axios";
-import * as LocalAuthentication from 'expo-local-authentication';
+import * as LocalAuthentication from "expo-local-authentication";
 
 export function InputLogin() {
   const [cpf, setCpf] = useState("");
@@ -47,22 +55,39 @@ export function InputLogin() {
         // Caso titular e checkbox não marcado
         await AsyncStorage.setItem("userCpf", cleanedCpf);
         await AsyncStorage.setItem("userId", userData.idAderente.toString());
-        await AsyncStorage.setItem("isTitular", userData.titularDoContrato.toString());
+        await AsyncStorage.setItem(
+          "isTitular",
+          userData.titularDoContrato.toString()
+        );
         router.push("/(tabs)/home");
       } else if (isDependente && !userData.titularDoContrato) {
         // Caso dependente e checkbox marcado
         await AsyncStorage.setItem("userCpf", cleanedCpf);
         await AsyncStorage.setItem("userId", userData.idAderente.toString());
-        await AsyncStorage.setItem("isTitular", userData.titularDoContrato.toString());
+        await AsyncStorage.setItem(
+          "isTitular",
+          userData.titularDoContrato.toString()
+        );
         router.push("/(tabs)/home");
       } else {
-        Alert.alert("Erro", "Não é possível acessar a aplicação com essas credenciais.");
-        console.log("Erro, não é possível acessar a aplicação com essas credenciais.");
+        Alert.alert(
+          "Erro",
+          "Não é possível acessar a aplicação com essas credenciais."
+        );
+        console.log(
+          "Erro, não é possível acessar a aplicação com essas credenciais."
+        );
       }
-
     } catch (error) {
-      if (error instanceof AxiosError && error.response && error.response.status === 400) {
-        Alert.alert("Erro", "Falha ao verificar o CPF. Por favor, verifique se as informações estão corretas.");
+      if (
+        error instanceof AxiosError &&
+        error.response &&
+        error.response.status === 400
+      ) {
+        Alert.alert(
+          "Erro",
+          "Falha ao verificar o CPF. Por favor, verifique se as informações estão corretas."
+        );
       } else {
         Alert.alert("Servidor fora do ar!", "Tente mais tarde!");
       }
@@ -70,13 +95,11 @@ export function InputLogin() {
     } finally {
       setLoading(false); // Para o carregamento
     }
-
-    
   };
 
-  const { width } = Dimensions.get('window');
+  const { width } = Dimensions.get("window");
 
-
+  /*
   // Parte que verifica a parte de autenticação nativa do apareelho
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
@@ -109,6 +132,54 @@ export function InputLogin() {
 
     console.log(auth);
 
+  }*/
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  async function verificaDisponibilidadeAutenticacao() {
+    const compatibilidade = await LocalAuthentication.hasHardwareAsync();
+    console.log("Compatibilidade de hardware para autenticação:", compatibilidade);
+
+    const tipoAutenticacao = await LocalAuthentication.supportedAuthenticationTypesAsync();
+    console.log("Tipos de autenticação suportados:", tipoAutenticacao.map(type => LocalAuthentication.AuthenticationType[type]));
+  }
+
+  async function handleAuthentication() {
+    const isAutenticacaoCadastrada = await LocalAuthentication.isEnrolledAsync();
+    console.log("Autenticação cadastrada:", isAutenticacaoCadastrada);
+
+    if (!isAutenticacaoCadastrada) {
+      return Alert.alert('Login', 'Nenhuma biometria ou face cadastrada. Por favor, cadastre no dispositivo!');
+    }
+
+    const supportedAuthTypes = await LocalAuthentication.supportedAuthenticationTypesAsync();
+    const isFaceRecognitionAvailable = supportedAuthTypes.includes(LocalAuthentication.AuthenticationType.FACIAL_RECOGNITION);
+    const isFingerprintAvailable = supportedAuthTypes.includes(LocalAuthentication.AuthenticationType.FINGERPRINT);
+
+    console.log("Reconhecimento facial disponível:", isFaceRecognitionAvailable);
+    console.log("Biometria disponível:", isFingerprintAvailable);
+
+    if (isFaceRecognitionAvailable) {
+      const auth = await LocalAuthentication.authenticateAsync({
+        promptMessage: "Login com Autenticação",
+        fallbackLabel: "Rosto não reconhecido",
+      });
+      console.log("Resultado da autenticação facial:", auth);
+      if (auth.success) {
+        handleLogin();
+      }
+    } else if (isFingerprintAvailable) {
+      const auth = await LocalAuthentication.authenticateAsync({
+        promptMessage: "Login com Autenticação",
+        fallbackLabel: "Biometria não reconhecida",
+      });
+      console.log("Resultado da autenticação biométrica:", auth);
+      if (auth.success) {
+        handleLogin();
+      }
+    } else {
+      Alert.alert('Login', 'Nenhuma biometria ou face cadastrada. Por favor, cadastre no dispositivo!');
+    }
   }
 
   useEffect(() => {
@@ -131,21 +202,41 @@ export function InputLogin() {
           status={isDependente ? "checked" : "unchecked"}
           onPress={() => setIsDependente(!isDependente)}
         />
-        <Text style={[styles.textoCheckBox, { fontSize: width * 0.04, marginRight: 10 }]}>Sou Dependente</Text>
+        <Text
+          style={[
+            styles.textoCheckBox,
+            { fontSize: width * 0.04, marginRight: 10 },
+          ]}
+        >
+          Sou Dependente
+        </Text>
         <FontAwesome6 name="person-circle-check" size={18} color="#3E4A59" />
       </View>
       <TouchableOpacity
         style={[styles.containerButtonEntrar, { padding: width * 0.02 }]}
         disabled={cpfExiste || loading} // Desabilita o botão durante o carregamento
-        onPress={handleAuthentication || handleLogin}
+        onPress={handleAuthentication}
       >
         {loading ? (
-          <ActivityIndicator size="small" color="#fff" style={[{marginBottom: -10, marginTop: 4}]} />
+          <ActivityIndicator
+            size="small"
+            color="#fff"
+            style={[{ marginBottom: -10, marginTop: 4 }]}
+          />
         ) : (
-          <Text style={[styles.textoButton, { fontSize: width * 0.05 }]}>Entrar</Text>
+          <Text style={[styles.textoButton, { fontSize: width * 0.05 }]}>
+            Entrar
+          </Text>
         )}
         {loading && (
-          <Text style={[styles.textoButton, { marginTop: 10, fontSize: width * 0.04 }]}>Carregando</Text>
+          <Text
+            style={[
+              styles.textoButton,
+              { marginTop: 10, fontSize: width * 0.04 },
+            ]}
+          >
+            Carregando
+          </Text>
         )}
       </TouchableOpacity>
     </View>
