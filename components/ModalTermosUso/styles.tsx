3d598d753689cf5794f047c565ebd0d436b9b7f9
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-07-11 11:22:11 -0300

    configuracao-termos-de-uso-0.2

--- components/ModalTermosUso/styles.tsx
+++ components/ModalTermosUso/styles.tsx
@@ -8,9 +8,10 @@ export const styles = StyleSheet.create({
     backgroundColor: "rgba(0,0,0,0.5)",
   },
   modalContent: {
-    width: "80%",
-    backgroundColor: "white",
-    borderRadius: 10,
+    width: "95%",
+    height: "85%",
+    backgroundColor: "#3E3D3D",
+    borderRadius: 4,
     padding: 20,
     alignItems: "center",
   },
@@ -18,19 +19,33 @@ export const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "bold",
     marginBottom: 10,
+    textAlign: 'center',
+    color: '#FFF'
   },
   content: {
     fontSize: 16,
     marginBottom: 20,
+    color: '#FFF',
+    paddingHorizontal: 15,
+    fontWeight: 'normal'
+    //textAlign: 'justify',
   },
   buttonContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
     width: "100%",
   },
-  button: {
+  buttonAceitar: {
     padding: 10,
     backgroundColor: "#007BFF",
+    borderRadius: 5,
+    margin: 5,
+    width: "45%",
+    alignItems: "center",
+  },
+  buttonNegar: {
+    padding: 10,
+    backgroundColor: "#F22",
     borderRadius: 5,
     margin: 5,
     width: "45%",
