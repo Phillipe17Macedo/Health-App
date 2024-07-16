078b2754e81901fd08efd1d2301917a72a088a98
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-07-16 14:53:56 -0300

    estilo-fonte-especialidade-dropdown

--- components/Consulta/DropDownEspecialidade/Especialidade.tsx
+++ components/Consulta/DropDownEspecialidade/Especialidade.tsx
@@ -3,6 +3,8 @@ import { View, Text } from "react-native";
 import DropDownPicker from "react-native-dropdown-picker";
 import { buscarEspecialidades } from "@/utils/requestConfig";
 import { styles } from "./styles";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
 interface EspecialidadeProps {
   EspecialidadeCarregada: (especialidadeId: string | null, especialidadeNome: string | null) => void;
@@ -47,6 +49,35 @@ export default function Especialidade({ EspecialidadeCarregada, especialidadeSel
     setValor(value);
   };
 
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadResourcesAndDataAsync() {
+      try {
+        SplashScreen.preventAutoHideAsync();
+
+        await Font.loadAsync({
+          "MPlusRounded1c-Medium": require("@/assets/fonts/M_PLUS_Rounded_1c/MPLUSRounded1c-Medium.ttf"),
+          "MPlusRounded1c-Regular": require("@/assets/fonts/M_PLUS_Rounded_1c/MPLUSRounded1c-Regular.ttf"),
+          "MPlusRounded1c-Bold": require("@/assets/fonts/M_PLUS_Rounded_1c/MPLUSRounded1c-Bold.ttf"),
+          "MPlusRounded1c-ExtraBold": require("@/assets/fonts/M_PLUS_Rounded_1c/MPLUSRounded1c-ExtraBold.ttf"),
+        });
+
+        setFontLoaded(true);
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        SplashScreen.hideAsync();
+      }
+    }
+
+    loadResourcesAndDataAsync();
+  }, []);
+
+  if (!fontLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <DropDownPicker
@@ -59,10 +90,10 @@ export default function Especialidade({ EspecialidadeCarregada, especialidadeSel
         setItems={setItens}
         placeholder="Selecione uma especialidade"
         style={styles.dropdown}
-        placeholderStyle={styles.textoDropdown}
+        placeholderStyle={[styles.textoDropdown, , {fontFamily: 'MPlusRounded1c-ExtraBold'}]}
         dropDownContainerStyle={styles.dropDownContainerStyle}
-        listItemLabelStyle={styles.itensLista}
-        selectedItemLabelStyle={styles.itemSelecionado}
+        listItemLabelStyle={[styles.itensLista, {fontFamily: 'MPlusRounded1c-ExtraBold'}]}
+        selectedItemLabelStyle={[styles.itemSelecionado, {fontFamily: 'MPlusRounded1c-ExtraBold'}]}
         zIndex={3000}
         zIndexInverse={1000}
       />
