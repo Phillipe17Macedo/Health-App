f7f74296672732b3ac577abb58619719f343fa80
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-07-29 09:50:15 -0300

    configurando-tela-perfil-para-receber-o-cartao

--- components/Perfil/EditarPerfil/Areadados/DadosUser.tsx
+++ components/Perfil/EditarPerfil/Areadados/DadosUser.tsx
@@ -1,88 +1,60 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, TextInput, Dimensions } from "react-native";
+import { View, RefreshControl, SafeAreaView, ScrollView } from "react-native";
 import { buscarAderente } from "@/utils/requestConfig";
-import { styles } from "./styles";
+import { StatusBar } from "expo-status-bar";
+import { Cartao } from "@/components/Perfil/EditarPerfil/cartaoPerfil/Cartao";
+import ModalCarregamento from "@/components/constants/ModalCarregamento";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { styles } from "./styles";
+
+interface User {
+  nome: string;
+  dataNasc: string;
+  tipoAdesao: string;
+  statusContrato: boolean;
+  titularDoContrato: boolean;
+  fotoBase64: string;
+}
 
 export default function DadosUser() {
-  const [nome, setNome] = useState<string>("");
-  const [cpfUser, setCpfUser] = useState<string>("");
-  const [dataNasc, setDataNasc] = useState<string>("");
-  const [loading, setLoading] = useState<boolean>(true);
+  const [user, setUser] = useState<User | null>(null);
+  const [refreshing, setRefreshing] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const { width } = Dimensions.get('screen');
+  const loadUser = async () => {
+    setLoading(true);
+    const userCpf = await AsyncStorage.getItem("userCpf");
+    const isTitular = await AsyncStorage.getItem("isTitular");
+
+    if (userCpf) {
+      const response = await buscarAderente(userCpf, isTitular === "true");
+      const userData: User | null = response.data;
+      setUser(userData);
+    }
+    setLoading(false);
+  };
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        const cpf = await AsyncStorage.getItem("userCpf");
-        const isTitular = await AsyncStorage.getItem("isTitular");
-
-        if (!cpf || isTitular === null) {
-          throw new Error("Dados de autenticação não encontrados");
-        }
-
-        const titular = isTitular === "true";
-
-        console.log("Buscando usuário com CPF:", cpf);
-        const response = await buscarAderente(cpf, titular);
-        console.log("Dados do usuário:", response); // Log dos dados do usuário
-        const usuario = response.data;
-        if (usuario) {
-          setNome(usuario.nome || "Não encontrado");
-          setCpfUser(usuario.cpf || "Não encontrado");
-          setDataNasc(usuario.dataNasc || "Não encontrado");
-        }
-      } catch (error) {
-        console.error("Erro ao buscar usuário:", error);
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    fetchData();
+    loadUser();
   }, []);
 
-  if (loading) {
-    return <Text>Carregando...</Text>;
-  }
-
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await loadUser();
+    setRefreshing(false);
+  };
   return (
     <>
-      <View style={[styles.container]}>
-        <View style={[styles.containerDescricao]}>
-          <Text style={[styles.descricaoDados, { marginLeft: width * 0.02 }]}>NOME COMPLETO:</Text>
-        </View>
-        <TextInput
-          placeholder="NOME COMPLETO"
-          style={[styles.dadosInput, { height: 'auto' }]}
-          keyboardType="default"
-          value={nome}
-          editable={false}
-          multiline={true}
-          numberOfLines={2} // Pode ajustar conforme necessário
-        />
-        <View style={[styles.containerDescricao]}>
-          <Text style={[styles.descricaoDados, { marginLeft: width * 0.02 }]}>CPF:</Text>
-        </View>
-        <TextInput
-          placeholder="CPF DO USUÁRIO"
-          style={[styles.dadosInput]}
-          keyboardType="default"
-          value={cpfUser}
-          editable={false}
-        />
-        <View style={[styles.containerDescricao]}>
-          <Text style={[styles.descricaoDados, { marginLeft: width * 0.02 }]}>DATA DE NASCIMENTO:</Text>
-        </View>
-        <TextInput
-          placeholder="DATA DE NASCIMENTO"
-          style={[styles.dadosInput]}
-          keyboardType="default"
-          value={dataNasc}
-          editable={false}
-        />
-      </View>
+      <SafeAreaView style={[styles.container]}>
+        <StatusBar style="auto" />
+        <ScrollView
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+        >
+          {user && <Cartao user={user} />}
+        </ScrollView>
+      </SafeAreaView>
     </>
   );
 }
