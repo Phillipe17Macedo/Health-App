79cb884d1f858504aead87c368a6c7b587157d67
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-08-07 14:20:42 -0300

    criando-componente-selecao-dependentes-guiaExame

---
+++ components/GuiaExame/ModalSelecaoDependentes/SelecaoDependente.tsx
@@ -0,0 +1,115 @@
+import React, { useEffect, useState } from "react";
+import { Modal, View, Text, TouchableOpacity } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import { styles } from "./styles";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+interface SelecaoDependenteProps {
+  visivel: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  isDependente: boolean;
+  setIsDependente: (value: boolean) => void;
+  dependentes: any[];
+  selectedDependente: string | null;
+  setSelectedDependente: (value: string | null) => void;
+}
+
+export default function SelecaoDependente({
+  visivel,
+  onClose,
+  onConfirm,
+  isDependente,
+  setIsDependente,
+  dependentes,
+  selectedDependente,
+  setSelectedDependente,
+}: SelecaoDependenteProps) {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadResourcesAndDataAsync() {
+      try {
+        SplashScreen.preventAutoHideAsync();
+
+        await Font.loadAsync({
+          "MPlusRounded1c-Medium": require("@/assets/fonts/M_PLUS_Rounded_1c/MPLUSRounded1c-Medium.ttf"),
+          "MPlusRounded1c-Regular": require("@/assets/fonts/M_PLUS_Rounded_1c/MPLUSRounded1c-Regular.ttf"),
+          "MPlusRounded1c-Bold": require("@/assets/fonts/M_PLUS_Rounded_1c/MPLUSRounded1c-Bold.ttf"),
+          "MPlusRounded1c-ExtraBold": require("@/assets/fonts/M_PLUS_Rounded_1c/MPLUSRounded1c-ExtraBold.ttf"),
+        });
+
+        setFontLoaded(true);
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        SplashScreen.hideAsync();
+      }
+    }
+
+    loadResourcesAndDataAsync();
+  }, []);
+
+  if (!fontLoaded) {
+    return null;
+  }
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={visivel}
+      onRequestClose={onClose}
+    >
+      <View style={styles.modalContainer}>
+        <View style={styles.modalContent}>
+          <Text
+            style={[
+              styles.modalTitle,
+              { fontFamily: "MPlusRounded1c-ExtraBold" },
+            ]}
+          >
+            Selecione o Dependente
+          </Text>
+          {isDependente && (
+            <Picker
+              selectedValue={selectedDependente}
+              onValueChange={(itemValue) => setSelectedDependente(itemValue)}
+              style={styles.picker}
+            >
+              {dependentes.map((dependente) => (
+                <Picker.Item
+                  key={dependente.nome}
+                  label={dependente.nome}
+                  value={dependente.nome}
+                />
+              ))}
+            </Picker>
+          )}
+          <View style={[styles.containerButtons]}>
+            <TouchableOpacity style={styles.confirmButton} onPress={onConfirm}>
+              <Text
+                style={[
+                  styles.confirmButtonText,
+                  { fontFamily: "MPlusRounded1c-Bold" },
+                ]}
+              >
+                Confirmar
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.cancelButton} onPress={onClose}>
+              <Text
+                style={[
+                  styles.cancelButtonText,
+                  { fontFamily: "MPlusRounded1c-Bold" },
+                ]}
+              >
+                Fechar
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+}
