d52174c89dd4da482c07438672be12b9eccbf9ec
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-08-07 14:19:34 -0300

    criando-dropdown-guia-exame

---
+++ components/GuiaExame/DropDownGuiaExame/DropDownExame.tsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import { View } from "react-native";
+import DropDownPicker from "react-native-dropdown-picker";
+import { styles } from "./styles";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+
+export default function DropDownGuiaExame() {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    { label: "Exame 1", value: "exame1" },
+    { label: "Exame 2", value: "exame2" },
+    { label: "Exame 3", value: "exame3" },
+    { label: "Exame 4", value: "exame4" },
+    { label: "Exame 5", value: "exame5" },
+    { label: "Exame 6", value: "exame6" },
+  ]);
+
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadResourcesAndDataAsync() {
+      try {
+        SplashScreen.preventAutoHideAsync();
+
+        await Font.loadAsync({
+          "MPlusRounded1c-Medium": require("@/assets/fonts/M_PLUS_Rounded_1c/MPLUSRounded1c-Medium.ttf"),
+          "MPlusRounded1c-Regular": require("@/assets/fonts/M_PLUS_Rounded_1c/MPLUSRounded1c-Regular.ttf"),
+          "MPlusRounded1c-Bold": require("@/assets/fonts/M_PLUS_Rounded_1c/MPLUSRounded1c-Bold.ttf"),
+          "MPlusRounded1c-ExtraBold": require("@/assets/fonts/M_PLUS_Rounded_1c/MPLUSRounded1c-ExtraBold.ttf"),
+        });
+
+        setFontLoaded(true);
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        SplashScreen.hideAsync();
+      }
+    }
+
+    loadResourcesAndDataAsync();
+  }, []);
+
+  return (
+    <>
+      <View style={styles.container}>
+        <DropDownPicker
+          open={open}
+          value={value}
+          items={items}
+          setOpen={setOpen}
+          setValue={setValue}
+          setItems={setItems}
+          placeholder="Selecione um Exame"
+          style={styles.dropdown}
+          placeholderStyle={[styles.textoDropdown, { fontFamily: 'MPlusRounded1c-ExtraBold' }]}
+          dropDownContainerStyle={styles.dropDownContainerStyle}
+          listItemLabelStyle={[styles.itensLista, { fontFamily: 'MPlusRounded1c-ExtraBold' }]}
+          selectedItemLabelStyle={[styles.itemSelecionado, { fontFamily: 'MPlusRounded1c-ExtraBold' }]}
+          zIndex={3000}
+          zIndexInverse={1000}
+        />
+      </View>
+    </>
+  );
+}
---
+++ components/GuiaExame/DropDownGuiaExame/styles.tsx
@@ -0,0 +1,42 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+const { height } = Dimensions.get('window');
+
+export const styles = StyleSheet.create({
+  container: {
+    marginTop: 45,
+    marginBottom: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90%',
+    alignSelf: 'center',
+  },
+  dropdown: {
+    backgroundColor: '#F8F8F6',
+    borderColor: '#65A693',
+    borderWidth: 0.5,
+    elevation: 2,
+    borderRadius: 20,
+  },
+  textoDropdown: {
+    color: '#025940',
+    fontSize: 15,
+  },
+  dropDownContainerStyle: {
+    backgroundColor: '#F8F8F6',
+    borderColor: '#65A693',
+    borderWidth: 0.5,
+    elevation: 2,
+    borderRadius: 20,
+    zIndex: height * 3000,
+    height: height * 0.27
+  },
+  itensLista: {
+    color: '#025940',
+    fontSize: 15,
+  },
+  itemSelecionado: {
+    color: '#8CBF1F',
+    fontSize: 15,
+  },
+});
