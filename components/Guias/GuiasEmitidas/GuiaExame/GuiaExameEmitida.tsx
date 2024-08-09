c10d23a800ad982456c73aa6025325984fc464f7
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-08-09 17:15:46 -0300

    criando-guia-exame-emitida

--- components/Guias/GuiasEmitidas/GuiaExame/GuiaExameEmitida.tsx
+++ components/Guias/GuiasEmitidas/GuiaExame/GuiaExameEmitida.tsx
@@ -1,34 +1,161 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, Alert, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { cancelarGuiaEmitida } from "@/utils/requestConfig";
+import { FontAwesome6, FontAwesome5 } from "@expo/vector-icons";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
-interface Exame {
-  id: number;
-  tipo: string;
-  data: string;
-  hora: string;
-  local: string;
+interface GuiaExame {
+  idGuia: number;
+  dataGuia: string;
+  aderente: string;
+  dependente: string | null;
+  laboratorio: string; // Essa propriedade é necessária
+  status: string;
+  vlrGuia?: number; // `vlrGuia` pode ser opcional
 }
 
 interface GuiaExameEmitidaProps {
-  exames: Exame[];
+  guias: GuiaExame[];
+  onGuiaCancelada: () => void;
 }
 
-const GuiaExameEmitida: React.FC<GuiaExameEmitidaProps> = ({ exames }) => {
+const GuiaExameEmitida: React.FC<GuiaExameEmitidaProps> = ({
+  guias,
+  onGuiaCancelada,
+}) => {
+  console.log("Guias recebidas no componente:", guias);
+
+  const handleCancel = async (idGuia: number) => {
+    try {
+      await cancelarGuiaEmitida(idGuia);
+      Alert.alert("Sucesso", "Guia de exame cancelada com sucesso.");
+      onGuiaCancelada();
+    } catch (error) {
+      Alert.alert(
+        "Erro",
+        "Não foi possível cancelar a guia de exame. Tente novamente mais tarde."
+      );
+    }
+  };
+
+  const GuiaItem: React.FC<{ guia: GuiaExame }> = ({ guia }) => {
+    const [fontLoaded, setFontLoaded] = useState(false);
+
+    useEffect(() => {
+      async function loadResourcesAndDataAsync() {
+        try {
+          SplashScreen.preventAutoHideAsync();
+
+          await Font.loadAsync({
+            "MPlusRounded1c-Medium": require("@/assets/fonts/M_PLUS_Rounded_1c/MPLUSRounded1c-Medium.ttf"),
+            "MPlusRounded1c-Regular": require("@/assets/fonts/M_PLUS_Rounded_1c/MPLUSRounded1c-Regular.ttf"),
+            "MPlusRounded1c-Bold": require("@/assets/fonts/M_PLUS_Rounded_1c/MPLUSRounded1c-Bold.ttf"),
+            "MPlusRounded1c-ExtraBold": require("@/assets/fonts/M_PLUS_Rounded_1c/MPLUSRounded1c-ExtraBold.ttf"),
+          });
+
+          setFontLoaded(true);
+        } catch (e) {
+          console.warn(e);
+        } finally {
+          SplashScreen.hideAsync();
+        }
+      }
+
+      loadResourcesAndDataAsync();
+    }, []);
+
+    if (!fontLoaded) {
+      return null;
+    }
+
+    return (
+      <View key={guia.idGuia} style={styles.item}>
+        <View style={[styles.constainerIcone]}>
+          <MaterialCommunityIcons name="flask-round-bottom" size={26} color="#1ED960" />
+          <Text style={[styles.textoIcone, { fontFamily: 'MPlusRounded1c-ExtraBold' }]}>Guia Laboratorial</Text>
+        </View>
+
+        <View style={[styles.containerTextoPadrao]}>
+          <FontAwesome6 name="person" size={21} color="#3E3D3D" />
+          <Text style={[styles.textoPadrao, { fontFamily: 'MPlusRounded1c-ExtraBold' }]}>Aderente:</Text>
+        </View>
+        <Text style={[styles.textoConfirmacao, , { fontFamily: 'MPlusRounded1c-Medium' }]}>
+          {guia.aderente}
+        </Text>
+
+        <View style={[styles.containerTextoPadrao]}>
+          <FontAwesome6 name="people-arrows" size={19} color="#3E3D3D" />
+          <Text style={[styles.textoPadrao, { fontFamily: 'MPlusRounded1c-ExtraBold' }]}>Dependente:</Text>
+        </View>
+        <Text style={[styles.textoConfirmacao, , { fontFamily: 'MPlusRounded1c-Medium' }]}>{guia.dependente || "N/A"}</Text>
+
+        <View style={[styles.containerTextoPadrao]}>
+          <FontAwesome5 name="notes-medical" size={19} color="#3E3D3D" />
+          <Text style={[styles.textoPadrao, { fontFamily: 'MPlusRounded1c-ExtraBold' }]}>Laboratório:</Text>
+        </View>
+        <Text style={[styles.textoConfirmacao, , { fontFamily: 'MPlusRounded1c-Medium' }]}>{guia.laboratorio}</Text>
+
+        <View style={[styles.containerTextoPadrao]}>
+          <FontAwesome5 name="calendar-alt" size={19} color="#3E3D3D" />
+          <Text style={[styles.textoPadrao, { fontFamily: 'MPlusRounded1c-ExtraBold' }]}>Data:</Text>
+        </View>
+        <Text style={[styles.textoConfirmacao, , { fontFamily: 'MPlusRounded1c-Medium' }]}>
+          {new Date(guia.dataGuia).toLocaleDateString(
+            "pt-BR",
+            {
+              timeZone: "UTC",
+            }
+          )}
+        </Text>
+
+        {guia.vlrGuia !== undefined && (
+          <>
+            <Text style={[styles.textoPadrao, { color: "#F22", marginBottom: 5, fontFamily: 'MPlusRounded1c-ExtraBold' }]}>
+              **ATENÇÃO PAGO NO LOCAL**
+            </Text>
+            <View style={[styles.containerTextoPadrao]}>
+              <FontAwesome5 name="money-bill-alt" size={19} color="#3E3D3D" />
+              <Text style={[styles.textoPadrao, { fontFamily: 'MPlusRounded1c-ExtraBold' }]}>Valor:</Text>
+            </View>
+            <Text style={[styles.textoConfirmacao, { fontFamily: 'MPlusRounded1c-Medium' }]}>R$ {guia.vlrGuia.toFixed(2)}</Text>
+          </>
+        )}
+
+        <View style={[styles.containerTextoPadrao]}>
+          <FontAwesome5 name="print" size={19} color="#3E3D3D" />
+          <Text style={[styles.textoPadrao, { fontFamily: 'MPlusRounded1c-ExtraBold' }]}>Status da Guia Emitida:</Text>
+        </View>
+        <Text style={[styles.textoConfirmacao, , { fontFamily: 'MPlusRounded1c-Medium' }]}>{guia.status}</Text>
+
+        <View style={styles.containerTempo}>
+          <TouchableOpacity style={[styles.containerButtonCancelar]}>
+            <Text
+              onPress={() => handleCancel(guia.idGuia)}
+              style={[styles.textoButtonCancelar, { fontFamily: 'MPlusRounded1c-ExtraBold' }]}
+            >
+              Cancelar Guia👆
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  };
+
+  if (guias.length === 0) {
+    return (
+      <View style={styles.emptyContainer}>
+        <Text style={[styles.emptyText, { fontFamily: 'MPlusRounded1c-Medium' }]}>Não há guias emitidas.</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      {exames.map((exame) => (
-        <View key={exame.id} style={styles.item}>
-          <View style={[styles.constainerIcone]}>
-            <MaterialCommunityIcons name="flask-round-bottom" size={28} color="#52D981" />
-            <Text style={[styles.textoIcone]}>Exames</Text>
-          </View>
-          <Text style={styles.text}>Tipo: {exame.tipo}</Text>
-          <Text style={styles.text}>Data: {exame.data}</Text>
-          <Text style={styles.text}>Hora: {exame.hora}</Text>
-          <Text style={styles.text}>Local: {exame.local}</Text>
-        </View>
+      {guias.map((guia) => (
+        <GuiaItem key={guia.idGuia} guia={guia} />
       ))}
     </View>
   );
--- components/Guias/GuiasEmitidas/GuiaExame/styles.tsx
+++ components/Guias/GuiasEmitidas/GuiaExame/styles.tsx
@@ -3,22 +3,37 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     padding: 10,
-    alignItems: 'center', 
-    marginBottom: 75,
+    alignItems: 'center',
+    marginBottom: 80,
   },
   item: {
     marginBottom: 10,
-    padding: 10,
+    paddingRight: 15,
+    paddingLeft: 15,
+    paddingTop: 15,
+    paddingBottom: 15,
     backgroundColor: '#F8F8F6',
-    borderRadius: 10,
+    borderRadius: 15,
     elevation: 3,
     width: '95%',
     borderWidth: 0.5,
     borderColor: '#B2B2B2'
   },
-  text: {
+  containerTextoPadrao: {
+    flexDirection: 'row',
+    width: "100%",
+  },
+  textoPadrao: {
     fontSize: 16,
-    color: '#3E3D3D',
+    marginVertical: 3,
+    color: "#3E3D3D",
+    marginLeft: 8,
+  },
+  textoConfirmacao: {
+    fontSize: 15,
+    marginVertical: 3,
+    marginBottom: 8,
+    color: "#03A66A"
   },
   constainerIcone :{
     flexDirection: 'row',
@@ -27,8 +42,60 @@ export const styles = StyleSheet.create({
   }, 
   textoIcone: {
     fontSize: 18,
-    color: "#52D981",
-    fontWeight: 'bold',
+    color: "#1ED960",
     marginLeft: 10,
+  },
+  conatinersButtons: {
+    flexDirection: 'row',
+    alignSelf: 'center',
+    justifyContent: 'space-evenly',
+    width: '100%',
+    //backgroundColor: 'gray',
+    marginTop: 12,
+    marginBottom: 5,
+  },
+  containerButtonCancelar: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F26938',
+    borderRadius: 60,
+    padding: 5,
+    height: 40,
+    width: '100%',
+    elevation: 3,
+  },
+  textoButtonCancelar: {
+    fontSize: 16,
+    color: '#fff',
+    textAlign: 'center',
+  },
+  emptyContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  emptyText: {
+    fontSize: 18,
+    color: '#888',
+  },
+  textoContainerTempo: {
+    fontSize: 13,
+    color: "#3E3D3D",
+    fontWeight: 'normal',
+  },
+  textoTempo: { 
+    fontSize: 14,
+    color: "#3E3D3D",
+    fontWeight: 'bold',
+  },
+  containerTempo: {
+    flexDirection: 'row',
+    backgroundColor: 'lightgray',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    marginTop: 10,
+    height: 60,
+    padding: 10,
+    borderRadius: 25,
   }
 });
