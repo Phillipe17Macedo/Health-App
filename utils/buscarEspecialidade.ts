84addd1669106dbdf6d00659f903a18dbb46c6e2
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-08-13 14:45:43 -0300

    implementando-metodo-token-especialidade-separado

--- utils/buscarEspecialidade.ts
+++ utils/buscarEspecialidade.ts
@@ -1,1 +1,30 @@
 import axiosConfig from "./constants/axiosConfig";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+// Função para buscar especialidades
+export async function buscarEspecialidades(): Promise<any> {
+  try {
+    const token = await AsyncStorage.getItem("userToken");
+
+    if (!token) {
+      throw new Error("token não encontrado no AsyncStorage.");
+    }
+
+    const configHeaderRequisicao = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const response = await axiosConfig.get(
+      "/Especialidades/GetEspecialidades",
+      configHeaderRequisicao
+    );
+    console.log("Especialidades:", response.data);
+
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao buscar especialidades:", error);
+    throw error;
+  }
+}
